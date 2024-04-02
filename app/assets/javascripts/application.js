5461964be8cd182255bde1ac194af8f6c80eb6f2
Ben.R <benraine@users.noreply.github.com>
2024-04-02 13:09:25 +0100

    Initial commit
    GitOrigin-RevId: 76ed389ba3ee30a4e9131fddd74e2c7ff7e18086

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,36 +4,5 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  <script src="https://cdn.jsdelivr.net/npm/@coreui/coreui@5.0.2/dist/js/coreui.bundle.min.js" integrity="sha384-/lLdeDXcg75fFKvNaXc6K+P80Jk8U+1mKJUYqkLSxqr18HIUvJYVN42+m23Zbw4b" crossorigin="anonymous"></script>
+  // Add JavaScript here
 })
-const myDateRangePickerCustomRanges = document.getElementById('myDateRangePickerCustomRanges')
-if (myDateRangePickerCustomRanges) {
-  const optionsCustomRanges = {
-    locale: 'en-US',
-    ranges: {
-      Today: [new Date(), new Date()],
-      Yesterday: [
-        new Date(new Date().setDate(new Date().getDate() - 1)),
-        new Date(new Date().setDate(new Date().getDate() - 1))
-      ],
-      'Last 7 Days': [
-        new Date(new Date().setDate(new Date().getDate() - 6)),
-        new Date(new Date())
-      ],
-      'Last 30 Days': [
-        new Date(new Date().setDate(new Date().getDate() - 29)),
-        new Date(new Date())
-      ],
-      'This Month': [
-        new Date(new Date().setDate(1)),
-        new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)
-      ],
-      'Last Month': [
-        new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1),
-        new Date(new Date().getFullYear(), new Date().getMonth(), 0)
-      ]
-    }
-  }
-
-  new coreui.DateRangePicker(myDateRangePickerCustomRanges, optionsCustomRanges)
-}
