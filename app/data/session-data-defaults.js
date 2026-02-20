ddd34b9bffbf3b2acf93c81f1ed5bf4e3f8ecc44
Asier de Quadra <asier.dequadra@engineering.digital.dwp.gov.uk>
2026-02-20 10:41:29 +0000

    Ur22 a11y
    GitOrigin-RevId: c0bc2e8203ce71be2f7b2bb3e36233c0396dd8fe

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -20,48 +20,49 @@ Example usage:
 module.exports = {
 
   // Insert values here
-  multicase : [
-    {
-      'other_parent': 'false',
-      'case_id': 'false',
-      'case_type': 'false',
-      'effective_from': '8 December 2014',
-      'effective_to': 'false'
-    },
-    {
-      'other_parent': 'Daria Oblonskaya',
-      'case_id': '1-2345999',
-      'case_type': 'Pay and collect',
-      'effective_from': '8 December 2014',
-      'effective_to': '6 May 2025',
-      'collection_charges': '5,444.55',
-      'regular_deduction_order': '20.00',
-      'lump_sum_deduction_order': '100.00',
-      'liability_order': '200.00',
-      'deduction_from_earning_order_or_request': '30.00',
-      'enforcements_cost': '160.00',
-      'csa_debt': '63.45',
-      'dna_cost': '87.23',
-      'subject_access_request_cost': '19.00',
-      'interest': '30.67',
-      'fees': '22.87',
-      'judicial_review_costs': '107.89',
-      'appeal_costs': '43.45',
-      'cms_admin_costs': '26.89',
-      'liability_order_admin_cost': '54.00',
-      'child_maintenance_expected': '10,070.52',
-      'amount_written_off': '0.00',
-      'cash_balance': '330.61',
-      'child_maintenance_payments_made': '7,387.06',
-      'charges_and_debt_payments_made': '76.91'
-    },
-    {
-      'other_parent': 'Ruth Slater',
-      'case_id': '1-2345703',
-      'case_type': 'Arrears only',
-      'effective_from': '21 December 2016',
-      'effective_to': '6 May 2025'
-    }
-  ]
+  multicase: {
+    'all_cases': '5',
+  },
+  receiving_parent_1: {
+    'other_parent': 'false',
+    'case_id': 'false',
+    'case_type': 'false',
+    'effective_from': '8 December 2014',
+    'effective_to': 'false'
+  },
+  receiving_parent_2: {
+    'other_parent': 'Daria Oblonskaya',
+    'case_id': '1-2345999',
+    'case_type': 'Pay and collect',
+    'effective_from': '8 December 2014',
+    'effective_to': '6 May 2025',
+    'collection_charges': '5,444.55',
+    'regular_deduction_order': '20.00',
+    'lump_sum_deduction_order': '100.00',
+    'liability_order': '200.00',
+    'deduction_from_earning_order_or_request': '30.00',
+    'enforcements_cost': '160.00',
+    'csa_debt': '63.45',
+    'dna_cost': '87.23',
+    'subject_access_request_cost': '19.00',
+    'interest': '30.67',
+    'fees': '22.87',
+    'judicial_review_costs': '107.89',
+    'appeal_costs': '43.45',
+    'cms_admin_costs': '26.89',
+    'liability_order_admin_cost': '54.00',
+    'child_maintenance_expected': '10,070.52',
+    'amount_written_off': '0.00',
+    'cash_balance': '330.61',
+    'child_maintenance_payments_made': '7,387.06',
+    'charges_and_debt_payments_made': '76.91'
+  },
+  receiving_parent_3: {
+    'other_parent': 'Ruth Slater',
+    'case_id': '1-2345703',
+    'case_type': 'Arrears only',
+    'effective_from': '21 December 2016',
+    'effective_to': '6 May 2025'
+  }
 }
 
