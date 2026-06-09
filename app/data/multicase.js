2f3ddd7377a0050293bb94d599200a25f64524fe
Asier de Quadra <asier.dequadra@engineering.digital.dwp.gov.uk>
2026-06-09 19:09:33 +0000

    2062 multicase
    GitOrigin-RevId: 0f1ca972542bab38a83d5397b290ed988c837021

--- app/data/multicase.js
+++ app/data/multicase.js
@@ -23,8 +23,10 @@ module.exports = {
         'case_index': '0',
         'case_status': 'active',
         'case_id': '1-2345999',
-        'service_type': 'Collect and pay',
+        'service_type': '--',
+        'sub_status': 'Contact and Gather Other Party Information',
         'effective_from': '7 October 2021',
+        'next_annual_review': '7 October 2026',
         'role': 'paying',
         'other_parent': 'Daria Oblonskaya',
         'qualifying_children': [
@@ -40,14 +42,20 @@ module.exports = {
             'overnight_stays': '0',
             'local_auth_stays': '0'
           }
-        ]
+        ],
+        'child_maintenance_expected': '1200.00',
+        'child_maintenance_paid': '800.00',
+        'charges_debt_expected': '120.00',
+        'charges_debt_paid': '10.00',
       },
       {
         'case_index': '1',
         'case_status': 'active',
         'case_id': '1-2245868',
-        'service_type': 'Collect and pay – Arrears only',
+        'service_type': 'Collect and pay',
+        'sub_status': 'Perform Calculation',
         'effective_from': '21 November 2022',
+        'next_annual_review': '21 November 2026',
         'role': 'paying',
         'other_parent': 'Ruth Slater',
         'qualifying_children': [
@@ -57,14 +65,20 @@ module.exports = {
             'overnight_stays': '0',
             'local_auth_stays': '0'
           }
-        ]
+        ],
+        'child_maintenance_expected': '3000.00',
+        'child_maintenance_paid': '2000.00',
+        'charges_debt_expected': '300.00',
+        'charges_debt_paid': '20.00',
       },
       {
         'case_index': '2',
         'case_status': 'active',
         'case_id': '1-2245868',
-        'service_type': 'Collect and pay – Arrears only',
+        'service_type': 'Collect and pay',
+        'sub_status': 'Arrears only',
         'effective_from': '28 June 2024',
+        'next_annual_review': '28 June 2026',
         'role': 'receiving',
         'other_parent': 'Ruth Slater',
         'qualifying_children': [
@@ -89,6 +103,30 @@ module.exports = {
         'date_closed': '3 Jun 2020',
         'case_id': '1-2311178',
         'other_parent': 'Ruth Slater'
+      },
+      {
+        'case_index': '5',
+        'case_status': 'closed',
+        'date_closed': '3 Jun 2026',
+        'case_id': '1-2245772',
+        'service_type': 'Collect and pay',
+        'sub_status': 'Arrears only',
+        'effective_from': '2 May 2012',
+        'next_annual_review': '2 May 2026',
+        'role': 'paying',
+        'other_parent': 'Zara Fleetwood',
+        'qualifying_children': [
+          {
+            'child_name': 'Thomas Fleetwood-Oblonsky',
+            'dob': '14 Aug 2010',
+            'overnight_stays': '2',
+            'local_auth_stays': '0'
+          }
+        ],
+        'child_maintenance_expected': '1000.00',
+        'child_maintenance_paid': '2000.00',
+        'charges_debt_expected': '100.00',
+        'charges_debt_paid': '20.00',
       }
     ]
   }
